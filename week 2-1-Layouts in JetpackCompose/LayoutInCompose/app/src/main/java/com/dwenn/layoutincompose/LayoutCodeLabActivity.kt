cc2f13ee928fe32a0d6e463f164f615e75adac2d
DwEnn <prgohn@gmail.com>
2021-11-20 23:58:32 +0900

    scaffold

--- week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/com/dwenn/layoutincompose/LayoutCodeLabActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/com/dwenn/layoutincompose/LayoutCodeLabActivity.kt
@@ -3,8 +3,13 @@ package com.dwenn.layoutincompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.dwenn.layoutincompose.ui.theme.LayoutInComposeTheme
 
 class LayoutCodeLabActivity: AppCompatActivity() {
@@ -21,12 +26,20 @@ class LayoutCodeLabActivity: AppCompatActivity() {
 
 @Composable
 fun LayoutCodeLab() {
-    Text(text = "Hi there !")
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Text(text = "Hi there!")
+            Text(text = "Thanks for going through the Layouts codelab")
+        }
+    }
 }
 
+@Preview
 @Composable
 fun LayoutCodeLabPreview() {
     LayoutInComposeTheme {
-
+        LayoutCodeLab()
     }
 }
