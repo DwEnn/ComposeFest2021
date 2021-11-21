ecf89aecad6e31f97bb27b049662e2082668be94
DwEnn <prgohn@gmail.com>
2021-11-21 21:25:01 +0900

    Layout In Compose - 2

--- week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/com/dwenn/layoutincompose/LayoutCodeLabActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutInCompose/app/src/main/java/com/dwenn/layoutincompose/LayoutCodeLabActivity.kt
@@ -3,16 +3,30 @@ package com.dwenn.layoutincompose
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.dwenn.layoutincompose.ui.theme.LayoutInComposeTheme
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
-class LayoutCodeLabActivity: AppCompatActivity() {
+class LayoutCodeLabActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +40,107 @@ class LayoutCodeLabActivity: AppCompatActivity() {
 
 @Composable
 fun LayoutCodeLab() {
-    Scaffold { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            Text(text = "Hi there!")
-            Text(text = "Thanks for going through the Layouts codelab")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+/**
+ * 재사용가능하게 composable func 구성
+ */
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
+//        SimpleList()
+//        LazyList()
+        val listSize = 100
+        val scrollState = rememberLazyListState()
+        ScrollControlButton(listSize = listSize, scrollState = scrollState)
+        ImageList(listSize, scrollState)
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #${it + 1}", modifier = Modifier.fillMaxWidth())
+        }
+    }
+}
+
+@Composable
+fun LazyList() {
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        items(100) {
+            Text("Item #${it + 1}", modifier = Modifier.fillMaxWidth())
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageList(listItem: Int, scrollState: LazyListState) {
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(index = it)
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ), contentDescription = "Android Logo", modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ScrollControlButton(listSize: Int, scrollState: LazyListState) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Row {
+        Button(onClick = {
+            coroutineScope.launch {
+                // 0 is the first item index
+                scrollState.animateScrollToItem(0)
+            }
+        }) {
+            Text(text = "Scroll to the top")
+        }
+        Button(onClick = {
+            coroutineScope.launch {
+                // listSize - 1 is the last index of the list
+                scrollState.animateScrollToItem(listSize - 1)
+            }
+        }) {
+            Text(text = "Scroll to the end")
         }
     }
 }
